0aa374fd53a93f7d20bb27fd9864d7e22e5ae12b
mustafa inaya <moose.inaya@icloud.com>
2019-02-23 16:24:01 -0300

    UPDATE.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 				<SmurfsForm />
 				<h1>SMURFS! 2.0 W/ Redux</h1>
 				<div>Welcome to your Redux version of Smurfs!</div>
-				<div>Start inside of your `src/index.js` file!</div>
+
 				<div>Have fun!</div>
 			</div>
 		);
